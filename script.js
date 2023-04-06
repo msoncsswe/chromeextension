374beadb68e867ee8fd45debeac8358f269d5a3c
msoncsswe <11yuuyuu26@gmail.com>
2023-04-06 02:00:53 -0400

    reassigning images

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-console.log('test');
-
-const image = document.getElementsByTagName('img');
-console.log(image);
+const imageObj = document.getElementsByTagName('img');
+console.log(imageObj);
+console.log(typeof imageObj);
 
 // if (image) {
 //   //
 // }
+for (const image in imageObj) {
+  imageObj[image].src = 'images/' + fileName;
+}
