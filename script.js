f95740759cf82a4813701f8ad59e8546bf4d3af3
Andrew <androoo.1997@gmail.com>
2023-04-06 17:43:14 -0400

    finished

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const imageObj = document.getElementsByTagName('img');
-// console.log(imageObj);
-// console.log(typeof imageObj);
+console.log(imageObj);
+console.log(typeof imageObj);
+console.log(Array.isArray(imageObj))
 
 // if (image) {
 //   //
 // }
 
-const arrOfImages = ["images/IMG_0463.png", "images/IMG_0503.png", "images/IMG_1050.png", "images/IMG_8493.png", "images/IMG_8652.png", "images/IMG_8669.png", "images/IMG_8980.png", "images/IMG_9018.png", "images/IMG_9098.png"]
-for (const image of imageObj) {
-//   image.src = 'IMG_0503.png';
-    // let url = chrome.extension.getURL('/images/IMG_8652.png')
-    image.src = chrome.runtime.getURL("/images/IMG_0463.png");
-  console.log(image)
+const arrOfImages = ["https://i.imgur.com/3mBNgqF.jpg", "https://i.imgur.com/y9m0sT9.jpg", "https://i.imgur.com/COi46sr.jpg", "https://i.imgur.com/G9rzVDc.jpg", "https://i.imgur.com/RxVmfef.jpg", "https://i.imgur.com/KKl7Mdh.jpg", "https://i.imgur.com/TfUvXmX.jpg"]
+
+const randomURL = () => {
+    return Math.floor(Math.random() * (arrOfImages.length - 1));
+  };
+  
+console.log(randomURL())
+
+ for (const image of imageObj) {
+        //   image.src = 'IMG_0503.png';
+            // let url = chrome.extension.getURL('/images/IMG_8652.png')
+            image.src = arrOfImages[randomURL()];
+        //   console.log(image)
 }
+
+
 
