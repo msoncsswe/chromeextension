da4f4458c7f84b862d4a2e31d25eef93a864c818
msoncsswe <11yuuyuu26@gmail.com>
2023-04-05 23:02:08 -0400

    image replacer

---
+++ background.js
@@ -0,0 +1,16 @@
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+let tab = getCurrentTab();
+tab
+  .then(function (success) {
+    const url = success.url;
+    // console.log(url);
+  })
+  .catch(function (error) {
+    console.log('error');
+  });
